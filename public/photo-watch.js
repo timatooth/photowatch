44e440617418b13d0d85c44eb9a24a0721c4a59b
Tim Sullivan <tim@timatooth.com>
2013-11-26 22:33:09 +1300

    added exif data displaying

--- public/photo-watch.js
+++ public/photo-watch.js
@@ -7,8 +7,36 @@
 var socket = io.connect();
 
 socket.on('new-photo', function(photo) {
-    $('.photoArea').prepend("<img src='"+photo+"' />").hide().fadeIn();
+
+    var photoBack = $('<div/>', {
+	class: 'photoBack',
+    }).prependTo('.photoArea');
+
+    var img = $('<img/>', {
+	//id: 'foo',
+	src: photo['url'],
+	//title: '',
+	class: 'photo',
+    }).appendTo(photoBack);
+
+    if(photo.exif){
+	var photoMeta = $('<div/>', {
+	    class: 'photoMeta',
+	}).appendTo(photoBack);
+	
+	var f = ((photo.exif.FNumber != null ) ? photo.exif.FNumber : "-");
+	var iso = ((photo.exif.ISO != null ) ? photo.exif.ISO : "-");
+	var s = ((photo.exif.ExposureTime != null ) ? photo.exif.ExposureTime : "-");
+	
+	$('<div/>', {
+	    html: s + "s <em>f</em> "+f+" ISO"+iso,
+	    class: 'photoMetaText',
+	}).appendTo(photoMeta);
+    }
+
+    
 });
+
 
 $(function(){
     
--- server.js
+++ server.js
@@ -8,8 +8,9 @@ var express = require('express'), app = express();
 var server = http.createServer(app).listen(3000);
 var jade = require('jade');
 var io = require('socket.io').listen(server);
-var watch = require('watch');
+var watch = require('watch'); //https://github.com/mikeal/watch
 var path = require('path');
+var ExifImage = require('exif').ExifImage; //https://github.com/gomfunkel/node-exif
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -46,18 +47,9 @@ watch.createMonitor(__dirname + '/public/photos',
 			monitor.on("created", function (f, stat) {
 			    // Handle new files
 			    //this is a work-around on osx for the event double firing bug.
-			    if (monitor.files[f] === undefined) {
-				var ext = path.extname(f);
-				var allowedExt = ['.jpg', '.png', '.gif'];
-				if(allowedExt.indexOf(ext) >= 0 ){
-				    var photo =  path.relative(path.join(__dirname, "public"), f);
-				    /**
-				       We need to set a timeout before sending the new-photo message because
-				       the watcher may have detected the new image before it has been completely
-				       written to disk. 
-				    */
-				    setTimeout(broadcastPhoto, 15000, photo);
-				}
+			    if (monitor.files[f] === undefined && isExtOk(f)) {
+				//
+				setTimeout(loadImageMeta, 1000, f);
 			    }
 			    
 			})
@@ -70,6 +62,52 @@ watch.createMonitor(__dirname + '/public/photos',
 		    })
 
 function broadcastPhoto(photo){
-    console.log("broadcasting photo: " + photo );
-    io.sockets.emit('new-photo', photo);
+    var toSend = {
+	url:  path.relative(path.join(__dirname, "public"), photo['filename']),
+	exif: photo.exif,
+	iptc: photo['iptc']
+    };
+    io.sockets.emit('new-photo', toSend);
+}
+
+function isExtOk(filename){
+    var ext = path.extname(filename);
+    var allowedExt = ['.jpg', '.png', '.gif'];
+    if(allowedExt.indexOf(ext) >= 0 ){
+	return true;
+    }
+    
+    return false;
+}
+
+/**
+* Build JSON data structure containing EXIF & IPTC information for transmission
+* @param filename The valid filename of image to fetch metadata.
+*/
+function loadImageMeta(filename) {
+    var photo = {
+	filename: filename,
+	exif: {},
+	iptc: null
+    };
+    
+    try {
+	new ExifImage({ image : filename }, function (error, exifData) {
+            if (error){
+		console.log('Error reading EXIF: ' + error.message);
+	    } else {
+		console.log(exifData);
+		photo.exif = {
+		    FNumber: exifData.exif['FNumber'],
+		    ISO: exifData.exif['ISO'],
+		    ExposureTime: Math.round(exifData.exif['ExposureTime'] * 100) / 100
+		};
+	    }
+	    broadcastPhoto(photo);
+	});
+    } catch (error) {
+	console.log('Caught EXIF Error: ' + error.message);
+    }
+    
+    //broadcastPhoto(photo);
 }
